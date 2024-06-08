36968f37c4bfa82a559a484d491254f70e60b063
hallowshaw <rhitam.rit54@gmail.com>
2024-06-08 15:34:09 +0530

    Added getUser in userController

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -52,3 +52,12 @@ export const logout = catchAsyncError(async (req, res, next) => {
             message: "Logged Out Successfully.",
         });
 });
+
+
+export const getUser = catchAsyncError((req, res, next) => {
+    const user = req.user;
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, logout } from '../controllers/userController.js';
+import { register, login, logout, getUser } from '../controllers/userController.js';
 import { isAuthorized } from './../middleware/auth.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", register)
 router.post("/login", login)
 router.get("/logout", isAuthorized, logout)
+router.get("/getuser", isAuthorized, getUser)
 
 export default router;
