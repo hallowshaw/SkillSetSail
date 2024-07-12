7eee56439b9c246f808fc94ca71994fb6fda8aa9
hallowshaw <rhitam.rit54@gmail.com>
2024-07-12 14:11:13 +0530

    Defined Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import "./App.css";
+import { Context } from "./main";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Home from "./components/Home/Home";
+import Jobs from "./components/Job/Jobs";
+import JobDetails from "./components/Job/JobDetails";
+import MyJobs from "./components/Job/MyJobs";
+import PostJobs from "./components/Job/PostJobs";
+import Application from "./components/Application/Application";
+import MyApplications from "./components/Application/MyApplications";
+import NotFound from "./components/NotFound/NotFound";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+
+  const {}
+
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/job/getall" element={<Jobs />} />
+          <Route path="/job/:id" element={<JobDetails />} />
+          <Route path="/job/post" element={<PostJobs />} />
+          <Route path="/job/me" element={<MyJobs />} />
+          <Route path="/application/:id" element={<Application />} />
+          <Route path="/application/me" element={<MyApplications />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <Toaster />
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- Frontend/src/components/Job/PostJob.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PostJob() {
-  return <div>PostJob</div>;
-}
-
-export default PostJob;
