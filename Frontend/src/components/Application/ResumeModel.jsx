068ea4d2c5198fec4267354f9692f0294916613a
hallowshaw <rhitam.rit54@gmail.com>
2024-07-13 03:22:01 +0530

    Added Application Submission

--- Frontend/src/components/Application/ResumeModel.jsx
+++ Frontend/src/components/Application/ResumeModel.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function ResumeModel() {
-  return <div>ResumeModel</div>;
-}
+const ResumeModel = ({ imageUrl, onClose }) => {
+  return (
+    <div className="resume-modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <img src={imageUrl} alt="resume" />
+      </div>
+    </div>
+  );
+};
 
 export default ResumeModel;
